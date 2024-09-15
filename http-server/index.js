714fb6097990ad0bd6e632020d8a62a38b1584e6
yamini749 <yaminisettipalli3@gmail.com>
2024-09-15 16:54:56 +0530

    Initial commit of the http-server project

---
+++ http-server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const minimist = require('minimist');
+
+// Parse command line arguments to get the port number
+const args = minimist(process.argv.slice(2));
+const port = args.port || 3000; // Default port is 3000 if no port argument is provided
+
+const app = express();
+
+// Serve home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+// Serve project page
+app.get('/projects', (req, res) => {
+  res.sendFile(path.join(__dirname, 'project.html'));
+});
+
+// Serve registration page
+app.get('/registration', (req, res) => {
+  res.sendFile(path.join(__dirname, 'registration.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
